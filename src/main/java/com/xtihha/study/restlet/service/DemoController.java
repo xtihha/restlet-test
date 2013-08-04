14fcfa37565b3b6dea48f599bc43921bfd06cd7a
zhangxiaohu <zhangxiaohu@meituan.com>
2013-08-04 15:52:43 +0800

    add help doc for /mvc/demo，currently there are 4 methods for test

--- src/main/java/com/xtihha/study/restlet/service/DemoController.java
+++ src/main/java/com/xtihha/study/restlet/service/DemoController.java
@@ -14,8 +14,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
+ * 尝试了四种方法
+ * <li>直接返回view，在spring-mvc-config.xml文件需要定义view的解析</li>
+ * <li>路径携带参数，类似xxx?p=v，依然寻找view返回</li>
+ * <li>rest风格路径，依然寻找view返回</li>
+ * <li>rest风格路径，直接调用Response相关返回写会，不寻找view</li>
+ * <p/>
  * User: zhangxiaohu
  * Date: 13-8-3
  * Time: 下午2:39
@@ -37,6 +46,17 @@ public class DemoController {
     private Annotation2Manager mgr2;
 
     @RequestMapping(value = "/demo", method = RequestMethod.GET)
+    public void help(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/demo1").append("\t").append("resolve by viewResolver").append("\n");
+        sb.append("/demo2?id=xx").append("\t").append("resolve by viewResolver").append("\n");
+        sb.append("/demo3/{id}").append("\t").append("resolve by viewResolver").append("\n");
+        sb.append("/demo4/{id}").append("\t").append("directly write through").append("\n");
+
+        res.getWriter().write(sb.toString());
+    }
+
+    @RequestMapping(value = "/demo1", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         logger.info("demo controller with spring-mvc annotation");
         model.addAttribute("message", "hello spring mvc annotation");
@@ -44,21 +64,31 @@ public class DemoController {
         mgr.hello();
         mgr2.hello2();
 
-        return "demo"; //demo.jsp
+        //ViewResolver会自动解析文件路径，增补为${prefix}folder/demo2${suffix}
+        // 其中一般prefix定义为/WEB-INF/jsp, suffix定义为.jsp
+        return "folder/demo2"; //demo2.jsp
     }
 
     @RequestMapping(value = "/demo2", method = RequestMethod.GET)
-    public String demoWithParam(@RequestParam("id") String demoId, ModelMap model) {
-        String ret = "get param, id=" + demoId;
+    public String demoWithParam2(@RequestParam("id") String demoId, ModelMap model) {
+        String ret = "get param2, id=" + demoId;
         model.addAttribute("message", ret);
-        return "demo";
+        return "folder/demo2";
     }
 
     @RequestMapping(value = "/demo3/{id}", method = RequestMethod.GET)
-    public String demoWithParam2(@PathVariable("id") String demoId, ModelMap model){
-        String ret = "get param with rest, id=" + demoId;
+    public String demoWithParam3(@PathVariable("id") String demoId, ModelMap model) {
+        String ret = "get param3 with rest, id=" + demoId;
         model.addAttribute("message", ret);
-        return "demo";
+        return "folder/demo2";
+    }
+
+    @RequestMapping(value = "/demo4/{id}", method = RequestMethod.GET)
+    public void demoWithParam4(@PathVariable("id") String demoId,
+                               HttpServletRequest request,
+                               HttpServletResponse response) throws IOException {
+        String ret = "get param4 with rest, id=" + demoId;
+        response.getWriter().write(ret);
     }
 
 }
